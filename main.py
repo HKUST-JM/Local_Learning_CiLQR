7e4e04e347706241a906f32c6ca2349272f315df
orandragon <c752664788@hotmail.com>
2022-02-14 13:35:39 -0800

    V2

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ from scenario.robotic_arm_tracking import RoboticArmTracking
 from utils.Logger import logger
 import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
-    for i in range(10):
-        try:
-            logger.set_folder_name("QuadCopter_" + str(i), remove_existing_folder=False).set_is_use_logger(True).set_is_save_json(True)
-            scenario = QuadCopter() 
-            nn_cilqr.NNiLQR(gaussian_noise_sigma=[[0.1], [0.1], [0.1], [0.1]], iLQR_max_iter=100).init(scenario).solve() 
-        except Exception as e:
-            pass
-        continue
-    scenario = QuadCopter() 
-    scenario.play("QuadCopter_9")
+# if __name__ == "__main__":
+#     for i in range(10):
+#         try:
+#             logger.set_folder_name("QuadCopter_" + str(i), remove_existing_folder=False).set_is_use_logger(True).set_is_save_json(True)
+#             scenario = QuadCopter() 
+#             nn_cilqr.NNiLQR(gaussian_noise_sigma=[[0.1], [0.1], [0.1], [0.1]], iLQR_max_iter=100).init(scenario).solve() 
+#         except Exception as e:
+#             pass
+#         continue
+#     scenario = QuadCopter() 
+#     scenario.play("QuadCopter_9")
 
-    logger.set_folder_name("QuadCopter_Log", remove_existing_folder=False).set_is_use_logger(True).set_is_save_json(True)
-    scenario = QuadCopter() 
-    log_barrier_ilqr.LogBarrieriLQR().init(scenario).solve() 
-    scenario.play("QuadCopter_Log")
+#     logger.set_folder_name("QuadCopter_Log", remove_existing_folder=False).set_is_use_logger(True).set_is_save_json(True)
+#     scenario = QuadCopter() 
+#     log_barrier_ilqr.LogBarrieriLQR().init(scenario).solve() 
+#     scenario.play("QuadCopter_Log")
 
 
 # if __name__ == "__main__":
@@ -40,21 +40,21 @@ if __name__ == "__main__":
 #     log_barrier_ilqr.LogBarrieriLQR().init(scenario).solve() 
 #     scenario.play("CarParking_Log")
 
-# if __name__ == "__main__":
-#     for i in range(10):
-#         try:
-#             logger.set_folder_name("RoboticArmTracking_" + str(i), remove_existing_folder=False).set_is_use_logger(True).set_is_save_json(True)
-#             scenario = RoboticArmTracking() 
-#             nn_cilqr.NNiLQR(gaussian_noise_sigma=[[0.1], [0.1]], iLQR_max_iter=100, training_stopping_criterion=0.01, decay_rate_max_iters=200).init(scenario).solve() 
-#         except Exception as e:
-#             pass
-#         continue
-#     scenario = RoboticArmTracking() 
-#     scenario.play("RoboticArmTracking_9")
+if __name__ == "__main__":
+    for i in range(10):
+        try:
+            logger.set_folder_name("RoboticArmTracking_" + str(i), remove_existing_folder=False).set_is_use_logger(True).set_is_save_json(True)
+            scenario = RoboticArmTracking() 
+            nn_cilqr.NNiLQR(gaussian_noise_sigma=[[0.1], [0.1]], iLQR_max_iter=100, training_stopping_criterion=0.01, decay_rate_max_iters=200).init(scenario).solve() 
+        except Exception as e:
+            pass
+        continue
+    scenario = RoboticArmTracking() 
+    scenario.play("RoboticArmTracking_9")
 
-#     logger.set_folder_name("RoboticArmTracking_log", remove_existing_folder=False).set_is_use_logger(True).set_is_save_json(True)
-#     scenario = RoboticArmTracking() 
-#     log_barrier_ilqr.LogBarrieriLQR().init(scenario).solve() 
-#     scenario.play("RoboticArmTracking_log")
-#     plt.show()
+    logger.set_folder_name("RoboticArmTracking_log", remove_existing_folder=False).set_is_use_logger(True).set_is_save_json(True)
+    scenario = RoboticArmTracking() 
+    log_barrier_ilqr.LogBarrieriLQR().init(scenario).solve() 
+    scenario.play("RoboticArmTracking_log")
+    plt.show()
 
